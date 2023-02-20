aa358c067c8a223e32bdebdb9709ec3a081270ec
Ric Colasanti <ricardo.colasanti@glasgow.ac.uk>
2023-02-20 13:32:28 +0000

    sysnking with ionio server

--- Test001.py
+++
@@ -1,6 +0,0 @@
-from schelling import Experiment
-experiment = Experiment(20)
-experiment.setUp(100)
-experiment.iterate()
-sim, happy, unhappy = experiment.getResultMatrix()
-print(sim, happy, unhappy)
